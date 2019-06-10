2b2a8998a37534a5255feaeccfc307071181a613
meehaw1337 <15misiek12@wp.pl>
2019-06-11 00:19:48 +0200

    please god let it just end

--- cw10/xd/client.c
+++ cw10/xd/client.c
@@ -15,116 +15,116 @@
 
 int init_unix(const char*);
 int init_inet(int,const char*);
-int client_loop(int, const char*);
 void exit_fun();
-void signal_fun(){
-  exit(1);
-}
+
+
 int sock_fd;
 
-int main(int argc, char **argv)
-{
-  char *hostname, *address;
+int main(int argc, char **argv) {
+    char *hostname, *unix_path;
 
-  atexit(exit_fun);
-  signal(SIGINT,signal_fun);
+    if(argc != 4 && argc != 5){
+        printf("Incorrect number of parameters, the program should be launched like this:\n");
+        printf("./client [name] [INET/UNIX] [UNIX file name / PORT IP]");
 
-  if(argc != 4 && argc != 5){
-    printf("ARGS: [NAME] [INET/UNIX] [NAME / (PORT IP)]\n");
-    return 1;
-  }
+        exit(1);
+    }
 
-  hostname = argv[1];
-  address  = argv[3];
-  if(strcmp(argv[2], "INET") == 0){
-    int port = atoi(address);
-    sock_fd = init_inet(port,argv[4]);
-  }else if(strcmp(argv[2], "UNIX") == 0){
-    sock_fd = init_unix(address);
+    atexit(exit_fun);
+
+    hostname = argv[1];
+    unix_path  = argv[3];
+    if(strcmp(argv[2], "INET") == 0){
+        int port = atoi(unix_path);
+        sock_fd = init_inet(port,argv[4]);
+    }
+    else if(strcmp(argv[2], "UNIX") == 0){
+        sock_fd = init_unix(unix_path);
+        }else{
+        printf("arg[2] has to be either INET or UNIX\n");
+        exit(1);
+    }
+
+    message msg;
+    char delim[] =  " \n\r\t\0";
+
+    msg.type = REGISTER;
+    strcpy(msg.text, hostname);
+    printf("%s\n",msg.text );
+    send(sock_fd, &msg, sizeof(msg), 0);
+
+    read(sock_fd,&msg,sizeof(msg));
+
+    if(msg.type == FAILED){
+        printf("Username already exists\n");
+        exit(1);
     }else{
-    printf("ARGS: [NAME] [INET/UNIX] [NAME / (PORT IP)]\n");
-    return 1;
-  }
+        printf("Registered to server\n");
+    }
 
-   message msg;
-   char delim[] =  " \n\r\t\0";
+    int index;
 
-   msg.type = REGISTER;
-   strcpy(msg.text, hostname);
-   printf("%s\n",msg.text );
-   send(sock_fd, &msg, sizeof(msg), 0);
-   memset(&msg,0,sizeof(message));
+    while(1){
+        if(read(sock_fd, &msg, sizeof(msg)) < 0)
+            continue;
 
-   read(sock_fd,&msg,sizeof(msg));
+        switch(msg.type) {
+            case PING:
+                break;
 
-   if(msg.type == FAILED){
-     printf("Username already exists\n");
-     exit(1);
-   }else{
-     printf("Registered to server\n");
-   }
-
-   int index;
-
-   while(1){
-     if(read(sock_fd, &msg, sizeof(msg)) < 0)
-       { continue; }
-
-     switch(msg.type)
-     {
-     case PING:
-       break;
-
-     case REQUEST:
-       for(int i = 0 ; i < MAX_WORDS ; ++i){
-         msg.words_counter[i]=0;
-         msg.words[i][0]='\0';
-       }
-       msg.counter = 0;
-       char* token;
+            case REQUEST:
+                for(int i = 0 ; i < MAX_WORDS ; i++){
+                    msg.words_counter[i]=0;
+                    strcpy(msg.words[i], "");
+                }
+                msg.counter = 0;
+                char* token;
 
 
-       token = strtok(msg.text,delim);
+                token = strtok(msg.text,delim);
 
-       while(token!=NULL){
-         ++msg.counter;
+                while(token!=NULL){
+                    ++msg.counter;
 
-         index = -1;
-         for(int i = 0 ; i < MAX_WORDS ; ++i){
-           if(strcmp(msg.words[i],token) == 0){
-             ++msg.words_counter[i];
-             index = i;
-             break;
-           }
-         }
+                    index = -1;
+                    for(int i = 0 ; i < MAX_WORDS ; i++){
+                        if(strcmp(msg.words[i],token) == 0){
+                            msg.words_counter[i]++;
+                            index = i;
+                            break;
+                        }
+                    }
 
-         if(index == -1){
-           for(int i = 0 ; i < MAX_WORDS ; ++i){
-             if(msg.words_counter[i]==0){
-               strcpy(msg.words[i],token);
-               ++msg.words_counter[i];
-               break;
-             }
-           }
-         }
+                    if(index == -1){
+                        for(int i = 0 ; i < MAX_WORDS ; i++){
+                            if(msg.words_counter[i]==0){
+                                strcpy(msg.words[i],token);
+                                msg.words_counter[i]++;
+                                break;
+                            }
+                        }
+                    }
 
-         token = strtok(NULL,delim);
-       }
+                    token = strtok(NULL,delim);
+                }
 
-       printf("COUNTED WORDS %d \n",msg.counter);
-       msg.type = RESPONSE;
-       if(send(sock_fd, &msg, sizeof(message), 0) < 0)
-         { printf("[SEND] ERROR\n"); }
-       break;
-     default:
-       break;
-     }
+                printf("Total words: %d \n", msg.counter);
+                msg.type = RESPONSE;
 
-     memset(&msg, 0, sizeof(message));
-   }
+                if(send(sock_fd, &msg, sizeof(message), 0) < 0) { 
+                        printf("Send failed\n"); 
+                }
 
-   close(sock_fd);
-   return 0;
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    close(sock_fd);
+    return 0;
 }
 
 void exit_fun(){
@@ -132,51 +132,46 @@ void exit_fun(){
 }
 
 
-int init_unix(const char * sockpath)
-{
-  int sock_fd;
-  struct sockaddr_un server_addr;
+int init_unix(const char * sockpath) { 
+    int sock_fd;
+    struct sockaddr_un server_addr;
 
-  sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-  if (sock_fd == -1)
-  {
-    printf("[UNIX] CREAT FAILURE\n");
-    exit(2);
-  }
+    sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock_fd == -1) {
+        printf("UNIX socket creation failed\n");
+        exit(2);
+    }
 
-  memset(&server_addr, 0, sizeof(server_addr));
-  server_addr.sun_family = AF_UNIX;
-  strcpy(server_addr.sun_path, sockpath);
+    server_addr.sun_family = AF_UNIX;
+    strcpy(server_addr.sun_path, sockpath);
 
-  if ((connect(sock_fd, (struct sockaddr*) &server_addr, sizeof(server_addr))) != 0)
-  {
-    printf("[UNIX] CONNECTION FAILURE\n");
-    exit(1);
-  }
+    if ((connect(sock_fd, (struct sockaddr*) &server_addr, sizeof(server_addr))) != 0) {
+        printf("UNIX connect failed\n");
+        exit(1);
+    }
 
-  return sock_fd;
+    return sock_fd;
 }
 
 
-int init_inet(int port,const char * ip)
-{
-  int sock_fd;
-  struct sockaddr_in server_addr;
+int init_inet(int port,const char * ip) {
+    int sock_fd;
+    struct sockaddr_in server_addr;
 
-  sock_fd = socket(AF_INET, SOCK_STREAM,0);
-  if (sock_fd == -1){
-     printf("[INET] CREAT FAILURE \n");
-      exit(1);
+    sock_fd = socket(AF_INET, SOCK_STREAM,0);
+    if (sock_fd == -1){
+        printf("INET socket creation failed\n");
+        exit(1);
     }
 
-  memset(&server_addr, 0, sizeof(server_addr));
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = inet_addr(ip);// htonl(INADDR_ANY);
-  server_addr.sin_port = htons(port);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server_addr.sin_port = htons(port);
 
-  if (connect(sock_fd, (struct sockaddr*) &server_addr, sizeof(server_addr)) != 0){
-     printf("[INET] CONNECTION FAILURE\n");
-    exit(1); }
+    if (connect(sock_fd, (struct sockaddr*) &server_addr, sizeof(server_addr)) != 0){
+        printf("INET connect failed\n");
+        exit(1);
+    }
 
-  return sock_fd;
+    return sock_fd;
 }
--- cw10/xd/server.c
+++ cw10/xd/server.c
@@ -23,414 +23,364 @@
 void *listener_thread(void *);
 void *input_thread(void *);
 void *ping_thread(void *);
-void handle_event(struct epoll_event*);
+void handle_event(struct epoll_event);
 
 void exit_handler(int);
 int register_client(int, const char*);
 
- int RUNNING = 1;
+int PORT;
+char UNIX_PATH[UNIX_PATH_MAX];
 
- int PORT;
- char UNIX_PATH[UNIX_PATH_MAX];
+int client_fd[MAX_CLIENTS] = {0};
+int client_state[MAX_CLIENTS] = {0};
+char client_name[MAX_CLIENTS][USERNAME_MAX_LEN];
 
- int client_fd[MAX_CLIENTS] = {0};
- int client_state[MAX_CLIENTS] = {0};
- char client_name[MAX_CLIENTS][USERNAME_MAX_LEN];
+int request = 0;
+int epoll_fd, unix_fd, inet_fd;
 
- int request = 0;
- int epoll_fd;
+int main(int argc, char **argv) {
+    pthread_t listener_tid, input_tid, ping_tid;
 
-int main(int argc, char **argv)
-{
-  pthread_t listener_tid, input_tid, ping_tid;
-
-  if(argc != 3){
-    printf("ARGS [PORT] [UNIX PORT NAME]\n");
-    
-    return 1;
-  }
-
-  PORT = atoi(argv[1]);
-  strcpy(UNIX_PATH, argv[2]);
-  signal(SIGINT, exit_handler);
-
-  if(pthread_create(&listener_tid, NULL, listener_thread, NULL) != 0){
-    return 1;
-  }
-
-  if(pthread_create(&input_tid, NULL, input_thread, NULL) != 0){
-    return 1;
-  }
-
-  if(pthread_create(&ping_tid, NULL, ping_thread, NULL) != 0){
-      return 1;
-  } 
-
-
-  if(pthread_join(listener_tid, NULL) != 0){
-    return 1;
-  }
-
-  if(pthread_join(input_tid, NULL) != 0){
-    return 1;
-  }
-
-  if(pthread_join(ping_tid, NULL) != 0){
-      return 1;
-  }
-
-  return 0;
-}
-
-void handle_event(struct epoll_event *event)
-{
-  message msg;
-
-  size_t bytes_read;
-  int fd;
-  int index = 0;
-
-  fd = event->data.fd;
-  bytes_read = read(fd, &msg, sizeof(message));
-  if(bytes_read == sizeof(message))
-  {
-    switch(msg.type)
-    {
-    case REGISTER:
-      if( (index = register_client(fd, msg.text)) >= 0){
-
-        msg.type=RESPONSE;
-        if(send(fd, &msg, sizeof(msg), 0) < 0){
-          printf("Couldnt send message to client\n" );
-          
-        }
-
-        printf("REGISTERED CLIENT %s\n",client_name[index]);
+    if(argc != 3){
+        printf("Incorrect number of arguments. The program should be launched like this: \n");
+        printf("./server [PORT] [UNIX file name]");
         
-      }else{
-
-        msg.type=FAILED;
-        if(send(fd, &msg, sizeof(msg), 0) < 0){
-          printf("Couldnt send message to client\n" );
-          
-        }
-      }
-
-      break;
-    case RESPONSE:
-      printf("RESPONSE %d\n", msg.id);
-      printf("Words: %d\n", msg.counter);
-
-
-      
-      for(int i = 0 ; i < MAX_WORDS ; ++i){
-          if(msg.words_counter[i] == 0){
-          break;
-          }
-
-
-        printf("%s : %d \n",msg.words[i],msg.words_counter[i] );
-        
-      }
-
-      for(int i=0; i < MAX_CLIENTS; i++)
-      {
-        if(client_fd[i] == fd)
-          {
-            printf("--------------- \nFROM:%s\n",client_name[i]);
-            client_state[i] = 0;
-            break; }
-      }
-
-      break;
-    default:
-      break;
+        exit(1);
     }
-  }
+
+    PORT = atoi(argv[1]);
+    strcpy(UNIX_PATH, argv[2]);
+    signal(SIGINT, exit_handler);
+
+    if(pthread_create(&listener_tid, NULL, listener_thread, NULL) != 0){
+        exit(1);
+    }
+
+    if(pthread_create(&input_tid, NULL, input_thread, NULL) != 0){
+        exit(1);
+    }
+
+    if(pthread_create(&ping_tid, NULL, ping_thread, NULL) != 0){
+        exit(1);
+    } 
+
+
+    if(pthread_join(listener_tid, NULL) != 0){
+        exit(1);
+    }
+
+    if(pthread_join(input_tid, NULL) != 0){
+        exit(1);
+    }
+
+    if(pthread_join(ping_tid, NULL) != 0){
+        exit(1);
+    }
+
+    return 0;
+}
+
+void handle_event(struct epoll_event event) {
+    message msg;
+
+    size_t bytes_read;
+    int fd;
+    int index = 0;
+
+    fd = event.data.fd;
+    bytes_read = read(fd, &msg, sizeof(message));
+
+    if(bytes_read > 0) {
+        switch(msg.type) {
+            case REGISTER:
+                if( (index = register_client(fd, msg.text)) >= 0) {
+
+                    msg.type=RESPONSE;
+                    if(send(fd, &msg, sizeof(msg), 0) < 0){
+                        printf("Couldn't send the message to the client\n" );
+                    }
+
+                    printf("Client: %s was successfully registered\n",client_name[index]);
+                    
+                }
+                else {
+
+                    msg.type=FAILED;
+                    if(send(fd, &msg, sizeof(msg), 0) < 0) {
+                        printf("Couldn't send the message to the client\n" );
+                    
+                    }
+                }
+
+                break;
+            case RESPONSE:
+                printf("Response number: %d\n", msg.id);
+                printf("Words: %d\n", msg.counter);
+
+                for(int i = 0 ; i < MAX_WORDS ; i++){
+                    if(msg.words_counter[i] == 0){
+                        break;
+                    }
+
+
+                    printf("%s : %d \n",msg.words[i],msg.words_counter[i] );
+                }
+
+                for(int i=0; i < MAX_CLIENTS; i++) {
+                    if(client_fd[i] == fd) {
+                        printf("--------------- \n From: %s\n\n",client_name[i]);
+                        client_state[i] = 0;
+                        break; 
+                    }
+                }
+
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 
-void *listener_thread(void *arg)
-{
-  int inet_fd, unix_fd, cli_fd;
-  struct sockaddr_in inet_addr, inet_cli;
-  struct sockaddr_un unix_addr, unix_cli;
-  socklen_t addr_len = sizeof(struct sockaddr);
+void *listener_thread(void *arg) {
+    int cli_fd;
+    struct sockaddr_in inet_addr, inet_cli;
+    struct sockaddr_un unix_addr, unix_cli;
+    socklen_t addr_len = sizeof(struct sockaddr);
 
-  int event_count;
-  struct epoll_event events[MAX_EPOLL_EVENTS];
+    int event_count;
+    struct epoll_event events[MAX_EPOLL_EVENTS];
 
-  if((inet_fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0))==-1){
-    printf("[INET] CREAT FAILURE\n");
-    
-    exit(1);
-  }
+    if((inet_fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0))==-1){
+        printf("INET socket creation failed\n");
+        
+        exit(1);
+    }
 
-  inet_addr.sin_family = AF_INET;
-  inet_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  inet_addr.sin_port = htons(PORT);
+    inet_addr.sin_family = AF_INET;
+    inet_addr.sin_addr.s_addr = INADDR_ANY;
+    inet_addr.sin_port = htons(PORT);
 
-  if ((bind(inet_fd, (struct sockaddr*) &inet_addr, sizeof(inet_addr))) != 0){
-    printf("[INET] BIND FAILURE\n");
-    
-    exit(2);
-  }
+    if ((bind(inet_fd, (struct sockaddr*) &inet_addr, sizeof(inet_addr))) != 0){
+        printf("INET bind failed\n");
+        
+        exit(2);
+    }
 
-  unix_fd = socket(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK, 0);
-  if (unix_fd == -1){
-      printf("[UNIX] CREAT FAILURE\n");
-      
-      exit(2);
-  }
+    unix_fd = socket(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK, 0);
+    if (unix_fd == -1){
+        printf("UNIX socket creation failed\n");
+        
+        exit(2);
+    }
 
-  unix_addr.sun_family = AF_UNIX;
-  strcpy(unix_addr.sun_path, UNIX_PATH);
+    unix_addr.sun_family = AF_UNIX;
+    strcpy(unix_addr.sun_path, UNIX_PATH);
 
-  if ((bind(unix_fd, (struct sockaddr*) &unix_addr, sizeof(unix_addr))) != 0){
-    printf("[UNIX] BIND FAILURE\n");
-    
-    exit(1);
-  }
+    if ((bind(unix_fd, (struct sockaddr*) &unix_addr, sizeof(unix_addr))) != 0){
+        printf("UNIX bind failed\n");
+        
+        exit(1);
+    }
 
-  if (listen(unix_fd, MAX_CLIENTS) != 0){
-    printf("[UNIX] LISTEN FAILURE\n");
-    
-    exit(1);
-  }
+    if (listen(inet_fd, MAX_CLIENTS) != 0){
+        printf("INET listen failed\n");
+        
+        exit(1);
+    }
 
-  if (listen(inet_fd, MAX_CLIENTS) != 0){
-    printf("[AF_INET] LISTEN FAILURE\n");
-    
-    exit(1);
-  }
+    if (listen(unix_fd, MAX_CLIENTS) != 0){
+        printf("UNIX listen failed\n");
+        
+        exit(1);
+    }
 
 
-  if((epoll_fd = epoll_create1(0)) == -1){
-    printf("[EPOLL] CREAT FAILURE\n");
-    
-    exit(1);
-  }
+    if((epoll_fd = epoll_create1(0)) == -1){
+        printf("Epoll descriptor creation failed\n");
+        
+        exit(1);
+    }
 
     struct epoll_event ev_unix;
-       ev_unix.events = EPOLLIN;
-       ev_unix.data.fd = unix_fd;
-    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, unix_fd, &ev_unix);
+    ev_unix.events = EPOLLIN;
+    ev_unix.data.fd = unix_fd;
+
+    if ( epoll_ctl(epoll_fd, EPOLL_CTL_ADD, unix_fd, &ev_unix) == -1) {
+        printf("Epoll_ctl failedn");
+        exit(1);
+    }
 
     struct epoll_event ev_inet;
-     ev_inet.events = EPOLLIN;
-     ev_inet.data.fd = inet_fd;
+    ev_inet.events = EPOLLIN;
+    ev_inet.data.fd = inet_fd;
 
-    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, inet_fd, &ev_inet);
-
-    struct timeval tv;
-  tv.tv_sec = 0;
-  tv.tv_usec = 100;
-
-
-
-  while(RUNNING){
-
-    /*cli_fd = accept(inet_fd, (struct sockaddr*) &inet_cli, &addr_len);
-    if(cli_fd >= 0)
-    {
-        setsockopt(cli_fd, SOL_SOCKET, SO_RCVTIMEO, (char*) &tv, sizeof(tv));
-      struct epoll_event event;
-       event.events = EPOLLIN | EPOLLOUT;
-       event.data.fd = cli_fd;
-
-       if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, cli_fd, &event) < 0)
-       {
-         printf("[EPOLL] ADD FAILURE\n");
-         exit(1);
-      }
+    if ( epoll_ctl(epoll_fd, EPOLL_CTL_ADD, inet_fd, &ev_inet) == -1) {
+        printf("Epoll_ctl failed\n");
+        exit(1);
     }
 
-    cli_fd = accept(unix_fd, (struct sockaddr*) &unix_cli, &addr_len);
-    if(cli_fd >= 0)
-    {
-        setsockopt(cli_fd, SOL_SOCKET, SO_RCVTIMEO, (char*) &tv, sizeof(tv));
-      struct epoll_event event;
-       event.events = EPOLLIN | EPOLLOUT;
-       event.data.fd = cli_fd;
+    while(1){
 
-       if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, cli_fd, &event) < 0)
-       {
-         printf("[EPOLL] ADD FAILURE\n");
-         exit(1);
-      }
-    }*/
+        event_count = epoll_wait(epoll_fd, events, MAX_EPOLL_EVENTS, 0);
 
+        for(int i = 0; i < event_count; i++) {
+            if(events[i].data.fd == inet_fd) {
+                cli_fd = accept(inet_fd, (struct sockaddr*) &inet_cli, &addr_len);
+                if(cli_fd >= 0) {
 
+                    struct epoll_event event;
+                    event.events = EPOLLIN;
+                    event.data.fd = cli_fd;
 
-    event_count = epoll_wait(epoll_fd, events, MAX_EPOLL_EVENTS, 0);
-
-    for(int i = 0; i < event_count; i++) {
-        if(events[i].data.fd == inet_fd) {
-            cli_fd = accept(inet_fd, (struct sockaddr*) &inet_cli, &addr_len);
-            if(cli_fd >= 0)
-            {
-                //setsockopt(cli_fd, SOL_SOCKET, SO_RCVTIMEO, (char*) &tv, sizeof(tv));
-                //int flags = fcntl(cli_fd, F_GETFL);
-                //fcntl(cli_fd, F_SETFL, flags | O_NONBLOCK);
-                struct epoll_event event;
-                event.events = EPOLLIN;
-                event.data.fd = cli_fd;
-
-                if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, cli_fd, &event) < 0)
-                {
-                    printf("[EPOLL] ADD FAILURE\n");
-                    exit(1);
+                    if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, cli_fd, &event) < 0) {
+                        printf("Epoll_ctl failed\n");
+                        exit(1);
+                    }
                 }
             }
-        }
-        else if(events[i].data.fd == unix_fd) {
-            cli_fd = accept(unix_fd, (struct sockaddr*) &unix_cli, &addr_len);
-            if(cli_fd >= 0)
-            {
-                //int flags = fcntl(cli_fd, F_GETFL);
-                //fcntl(cli_fd, F_SETFL, flags | O_NONBLOCK);
-                //setsockopt(cli_fd, SOL_SOCKET, SO_RCVTIMEO, (char*) &tv, sizeof(tv));
-                struct epoll_event event;
-                event.events = EPOLLIN;
-                event.data.fd = cli_fd;
+            else if(events[i].data.fd == unix_fd) {
+                cli_fd = accept(unix_fd, (struct sockaddr*) &unix_cli, &addr_len);
+                if(cli_fd >= 0) {
 
-                if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, cli_fd, &event) < 0)
-                {
-                    printf("[EPOLL] ADD FAILURE\n");
-                    exit(1);
+                    struct epoll_event event;
+                    event.events = EPOLLIN;
+                    event.data.fd = cli_fd;
+
+                    if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, cli_fd, &event) < 0) {
+                        printf("Epoll_ctl failed\n");
+                        exit(1);
+                    }
                 }
             }
-        }
-        else
-            handle_event(&events[i]);
+            else
+                handle_event(events[i]);
 
+        }
+
+        
     }
 
-    
-  }
-
-  close(inet_fd);
-  close(unix_fd);
-  close(epoll_fd);
-  unlink(UNIX_PATH);
-
-  pthread_exit(NULL);
+    pthread_exit(NULL);
 }
 
 void *ping_thread(void *arg) {
     message msg_ping;
 
-    while(RUNNING){
+    while(1){
         for(int i=0; i<MAX_CLIENTS; i++) {
             if(client_fd[i] != 0) {
                 msg_ping.type = PING;
 
-                if(send(client_fd[i], &msg_ping, sizeof(msg_ping), 0) < 0) {
+                if(send(client_fd[i], &msg_ping, sizeof(msg_ping), MSG_NOSIGNAL) < 0) {
                     printf("Client %s is not responding\n", client_name[i]);
 
                     struct epoll_event event;
                     event.data.fd = client_fd[i];
                     epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd[i], &event);
                     
-                    client_fd[i] = client_state[i] = 0;
+                    client_fd[i] = 0;
+                    client_state[i] = 0;
                     strcpy(client_name[i], "");
                 }
             }
         }
+        sleep(1);
     }
 
     pthread_exit(NULL);
 }
 
 void *input_thread(void *arg) {
-  message msg;
-  int fp;
+    message msg;
+    int fd;
 
-  msg.type = REQUEST;
+    msg.type = REQUEST;
 
-  char filename[512];
+    char filename[512];
 
-  while(RUNNING) {
-    scanf("%s", filename);
+    while(1) {
+        scanf("%s", filename);
 
-    fp = open(filename, O_RDONLY);
+        fd = open(filename, O_RDONLY);
 
-      if(fp < 0){
-          printf("open error\n");
-          continue;
-      }
+        if(fd < 0){
+            printf("File not found\n");
+            continue;
+        }
 
-      if(read(fp, msg.text, sizeof(msg.text)) < 0){
-          continue;
-      }
+        if(read(fd, msg.text, sizeof(msg.text)) < 0){
+            continue;
+        }
 
-      close(fp);
+        close(fd);
 
-      msg.id = request++;
+        msg.id = request++;
 
-      int cli_idx, cli_fd;
-      cli_idx = -1;
+        int cli_id, cli_fd;
+        cli_id = -1;
 
-      for(int i=0; i<MAX_CLIENTS; i++){
-        if(client_fd[i] == 0)
-          { continue; }
+        for(int i=0; i<MAX_CLIENTS; i++){
+            if(client_fd[i] != 0 && client_state[i] == 0){
+                cli_id = i;
+                break;
+            }
+            
+        }
 
-        cli_idx = i;
-        if(!client_state[i])
-          { break; }
-      }
+        if(cli_id == -1){
+            printf("No availablie clients\n");
+            continue;
+        }
 
-      cli_fd = client_fd[cli_idx];
+        cli_fd = client_fd[cli_id];
 
-      if(cli_idx == -1 || cli_fd == 0){
-         printf("No availablie clients\n");
-         continue;
-       }
-       printf("sending: %s\n",msg.text);
+        printf("Sending: %s\n",msg.text);
 
-      if(send(cli_fd, &msg, sizeof(msg), 0) < 0){
-         printf("[SENDING] TO %d ERROR\n", cli_fd);
-      }
-      else{
-        client_state[cli_idx] = 1;
-      }
+        if(send(cli_fd, &msg, sizeof(msg), 0) < 0){
+            printf("Send failed\n");
+        }
+        else{
+            client_state[cli_id] = 1;
+        }
 
-  }
-
-  pthread_exit(NULL);
-}
-
-void exit_handler(int sig)
-{
-  printf("shutdown\n");
-  RUNNING = 0;
-    for(int i=0; i<MAX_CLIENTS; i++){
-    if(client_fd[i] != 0)
-      shutdown(client_fd[i], SHUT_RDWR);
-  }
-  sleep(1);
-  exit(0);
-}
-
-
-int register_client(int fd, const char *hostname)
-{
-
-  for( int i = 0 ; i < MAX_CLIENTS ; ++i){
-    if(strcmp(client_name[i],hostname) == 0 )
-      return -1;
-  }
-
-  for( int i = 0 ; i < MAX_CLIENTS ; ++i){
-    if(client_fd[i] ==0){
-      client_fd[i] = fd;
-      client_state[i] = 0;
-      strcpy(client_name[i], hostname);
-      return i;
     }
-  }
 
-  return -2;
+    pthread_exit(NULL);
+}
+
+void exit_handler(int sig) {
+    printf("Shutdown\n");
+    for(int i=0; i<MAX_CLIENTS; i++){
+        if(client_fd[i] != 0)
+            shutdown(client_fd[i], SHUT_RDWR);
+    }
+    close(inet_fd);
+    close(unix_fd);
+    close(epoll_fd);
+    unlink(UNIX_PATH);
+
+    exit(0);
+}
+
+
+int register_client(int fd, const char *hostname){
+
+    for( int i = 0 ; i < MAX_CLIENTS ; i++){
+        if(strcmp(client_name[i],hostname) == 0 )
+        return -1;
+    }
+
+    for( int i = 0 ; i < MAX_CLIENTS ; i++){
+        if(client_fd[i] ==0){
+        client_fd[i] = fd;
+        client_state[i] = 0;
+        strcpy(client_name[i], hostname);
+        return i;
+        }
+    }
+
+    return -2;
 }
 
 
